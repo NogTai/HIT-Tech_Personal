3c2f3dcdbdbadea80dddeb8ae12bf3ea77196c1c
NogTai <thetai2382002@gmail.com>
2022-11-30 00:46:03 +0700

    add comment code

--- Trending/main.py
+++ Trending/main.py
@@ -3,5 +3,9 @@ import pandas as pd
 
 if __name__ == "__main__":
     Elo_Score = pd.read_csv('fakeData.csv')
+
+    # TieuChiChinh: Tên các cột dùng để xét tiêu chí chính giúp tính điểm Elo
+    # TieuChiPhu: Tên các cột dùng để xét tiêu chí phụ khi điểm Elo bằng nhau
+    # K: hệ số quan trọng của lần tính toán lại Elo này
     New_data_sort_by_Elo = Calculate_Elo(Elo_Score.copy(), TieuChiChinh=['Home_Work_Score', 'Attendance_Score', 'Rate'], TieuChiPhu=['Time'], K=12)
     print(New_data_sort_by_Elo.Calculate())
